1ed35f4892dcebde18b1277564dad1bbe7af5e81
Nathan <poppyandnunubot@gmail.com>
2024-05-20 23:43:42 +0100

    Finished inventroy and keys

---
+++ src/GameDataSingleton.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+enum item_types {KEY, CONSUMABLE, TOOL}
+
+var itemLookupTable = {
+	1 : {
+		"name" : "Small Key",
+		"type" : item_types.KEY
+	}
+}
--- src/interactables/pickup_action.gd
+++ src/interactables/pickup_action.gd
@@ -1,13 +1,13 @@
 extends InteractableAction
-
+class_name PickupAction
 var inventory = load("res://_PROTO_/inventroy.tres")
 var itemData
-@export var type : GameDataSingleton.item_types
+
+@export var id : int = 1
 
 func _on_interact():
-	itemData = interactable.toDict()
-	#double stores the ID but that isnt the worst thing i think.
-	inventory.bags[type][itemData["id"]] = itemData
+	inventory.add(id)
+	print(inventory.bags)
 	interactable.queue_free()
-	print(inventory.bags[type])
+
 
