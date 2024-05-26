9a701709d58694d9697edd8b434c0776671bb0c9
Nathan <poppyandnunubot@gmail.com>
2024-05-26 12:22:07 +0100

    Added healing and max hp drain to healthpool

--- src/health_pool.gd
+++ src/health_pool.gd
@@ -3,14 +3,16 @@ extends Node
 class_name HealthPool
 
 @export var max_hp : float = 10
-
+@export var max_hp_loss_ration : float = 0.01
 @export var hurtboxes : Array[Hurtbox]
 
 @onready var parent = $".."
 
+var curr_max_hp : float
 var curr_hp : float
 
 func _ready():
+	curr_max_hp = max_hp
 	curr_hp = max_hp
 	if hurtboxes == null:			# bind hurtboxes
 		queue_free()
@@ -21,6 +23,11 @@ func _ready():
 
 func hurt(di : DamageInstance):
 	curr_hp -= di.damage
+	curr_max_hp -= di.damage * max_hp_loss_ration
 	if curr_hp <= 0.0 :
 		parent.die()
-	print(str("someone's health pool is now ", curr_hp))
+	#print(str("someone's health pool is now ", curr_hp))
+	
+
+func heal(healing : float):
+	curr_hp = minf(curr_max_hp,curr_hp+healing)
