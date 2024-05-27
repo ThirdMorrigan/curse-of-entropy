17b691adbe1f90b2883270d2fac3d9897f22e542
MatthewHinchliffe <mjhinchliffe1@gmail.com>
2024-05-27 23:05:56 +0100

    Footsept noise

--- src/Audio control node.gd
+++ src/Audio control node.gd
@@ -1,6 +1,9 @@
 extends Node3D
 @onready var music : AudioStreamPlayer = $music
 
+@onready var player_sfx : AudioStreamPlayer = $player_sfx
+
+@onready var player = $"../../.."
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,7 +12,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	player_moving()
 
 
 func _on_new_room_entered(roomData : Dictionary):
@@ -18,3 +21,11 @@ func _on_new_room_entered(roomData : Dictionary):
 		GameDataSingleton.map_zone.GARDEN:
 			music.play()
 		# GameDataSingleton.map_zone.GARDEN:
+
+
+func player_moving():
+	if player.velocity.length_squared()>0.001 && player.is_on_floor():
+		if !player_sfx.playing:
+			player_sfx.play()
+	else:
+		player_sfx.stop()
