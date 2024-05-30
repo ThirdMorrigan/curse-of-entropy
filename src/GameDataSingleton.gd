36e29d7ac3112b91bc55f637f529909a029d1cc7
Nathan <poppyandnunubot@gmail.com>
2024-05-30 18:25:17 +0100

    new item pickups, new world map, burnable walls

--- src/GameDataSingleton.gd
+++ src/GameDataSingleton.gd
@@ -1,9 +1,12 @@
+@tool
 extends Node
 
 
 enum item_types {KEY, CONSUMABLE, TOOL}
 enum map_zone {GARDEN,GRAVEYARD,CASTLE,CRYPT,DUNGEON,CAVE,SEWER}
 enum equipment_slots {TOOL, HEAD, BODY, FEET, HANDS}
+
+var key_visuals = "res://scenes/visuals/key_visuals.tscn"
 
 var itemLookupTable = {
 	0 : {
@@ -21,15 +24,25 @@ var itemLookupTable = {
 	3 : {
 		"name" : "Pickaxe",
 		"type" : item_types.TOOL,
-		"slot" : equipment_slots.TOOL
+		"slot" : equipment_slots.TOOL,
+		"visuals" : "res://scenes/visuals/pickaxe_visuals.tscn"
 	},
 	4 : {#key for the cell in the tutorial
-		"name" : "Cell key",
-		"type" : item_types.KEY
+		"name" : "South west courtyard key",
+		"type" : item_types.KEY,
+		"description" : "A key found in the fountain of the courtyard,
+		the base of the key depicts a compass pointing to the south west",
+		"visuals" : key_visuals
 	},
 	5 : {#key for the exit of the tutorial
 		"name" : "Cave emergency exit key",
-		"type" : item_types.KEY
+		"type" : item_types.KEY,
+		"visuals" : key_visuals
 	},
+	6 : {
+		"name" : "Small health potion",
+		"type" : item_types.CONSUMABLE,
+		"visuals" : "res://scenes/visuals/potion_visuals.tscn"
+	}
 	
 }
