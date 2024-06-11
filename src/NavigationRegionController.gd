17944388ca75fc5c39769c33e97cedfd8893b289
Nathan <poppyandnunubot@gmail.com>
2024-06-11 22:56:54 +0100

    spawner less shit

--- src/NavigationRegionController.gd
+++ src/NavigationRegionController.gd
@@ -1,6 +1,8 @@
 extends NavigationRegion3D
 class_name NavigationRegionController
 var func_godot_map
+var baking : bool = false
+var queue : bool = false
 
 func _ready():
 	func_godot_map = $"..".find_children("*", "FuncGodotMap")[0]
@@ -10,9 +12,16 @@ func _ready():
 	bake_finished.connect(_on_bake_finished)
 
 func _reload_nav_mesh():
+	if baking:
+		queue = true
 	bake_navigation_mesh(true)
+	baking = true
+	
 
 
 
 func _on_bake_finished():
 	print("new nav mesh generated")
+	baking = false
+	if queue:
+		_reload_nav_mesh()
