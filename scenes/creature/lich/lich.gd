53b60370d473041af663a3baa8ce7704b6a91772
Ciara <147958656+ThirdMorrigan@users.noreply.github.com>
2024-06-15 01:38:32 +0100

    final boss fine tuning

--- scenes/creature/lich/lich.gd
+++ scenes/creature/lich/lich.gd
@@ -9,8 +9,9 @@ class_name Lich
 		return shielded
 	set(s):
 		shielded = s
-		$metarig/Skeleton3D/Cube.get_surface_override_material(0).set("hide", s)
-		$HealthPool.immunities = 0b11111 if s else 0b11101
+		$metarig/Skeleton3D/Cube.get_surface_override_material(0).set("shader_parameter/hide", !s)
+		$HealthPool.immunities = 0b11111 if s else 0b0
+		$sheild/Hurtbox.monitorable = s
 		
 var player : Player :
 	get :
@@ -19,6 +20,8 @@ var player : Player :
 		player = p
 		if anim_tree != null:
 			anim_tree.player = player
+
+signal victory
 
 var process : bool
 
@@ -30,29 +33,39 @@ var current_attack : Attack
 func wake():
 	$metarig.visible = true
 	$spawn.emitting = true
+	anim_tree.reset = false
 	anim_tree.wake = true
+	shielded = true
+	$Hurtbox.monitorable = true
 	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	shielded = false
 	$metarig.visible = false
 	process = false
 	$player_check.area_entered.connect(_on_area_entered)
 	anim_tree.animation_finished.connect(_on_animation_finished)
 	anim_tree.fire.connect(_on_fire)
+	$Hurtbox.monitorable = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if process:
+		scale = Vector3(2.0,2.0,2.0)
 		attack_timer -= delta
 		var to_player = player.global_position - global_position
-		global_rotation.y = atan2(player.global_position.x, player.global_position.z)
+		global_rotation.y = lerp_angle(global_rotation.y, atan2(-to_player.x, -to_player.z), delta * 2.0)
 		if attack_timer <= 0.0 && current_attack == null:
-			if to_player.length_squared() > 3.5 * 3.5:
+			print(to_player.length())
+			if to_player.length_squared() < 6 * 6:
 				anim_tree.attack_horizontal = true
+				current_attack = $swing_horizontal
 			else :
 				anim_tree.attack_cast = true
+				current_attack = $magic_cast
+		scale = Vector3(2.0,2.0,2.0)
 	
 
 func _on_area_entered(hb : Area3D):
@@ -63,14 +76,33 @@ func _on_area_entered(hb : Area3D):
 	
 func _on_animation_finished(anim_name : String):
 	anim_name = anim_name.to_upper()
-	if anim_name.contains("spawn"):
+	if anim_name.contains("SPAWN"):
 		process = true
-	elif anim_name.contains("attack"):
+	elif anim_name.contains("ATTACK"):
+		
 		current_attack = null
+	elif anim_tree.contains("SPAWN") && anim_tree.dying:
+		print("wawa")
+		victory.emit()
 
 func _on_fire():
 	current_attack.fire()
 	attack_timer = current_attack.wind_down
 
 func die():
-	pass
+	print("epic win!!!!")
+	process = false
+	anim_tree.dying = true
+	#queue_free()
+
+func stop():
+	process = false
+
+func delete():
+	shielded = false
+	process = false
+	attack_timer = 3.0
+	anim_tree.reset = true
+	$metarig.visible = false
+	$Hurtbox.monitorable = false
+	
